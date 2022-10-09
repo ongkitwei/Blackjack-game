4389b39cb899d9c21e0453cc2332267d5f5d2d24
KIT WEI <ongkitwei@gmail.com>
2022-10-09 22:01:51 +0800

    test

---
+++ index.js
@@ -0,0 +1,116 @@
+/*let firstCard = 5;
+let secondCard = 7;
+
+const sum = firstCard + secondCard ;
+
+
+if (sum > 21)
+{
+    console.log ("BURST! 😓") ; 
+}
+
+
+
+else if (sum === 21)
+{
+    console.log("congrats.. you won! 😁");
+}
+
+
+
+else if ( sum < 21)
+{
+    console.log("Do you wanna draw a new card? 😠");
+} */
+
+
+
+/*let age = 100;
+
+if (age < 100)
+{
+    console.log("not elegible");
+}
+
+
+else if (age === 100)
+{
+    console.log("here is your brithday card from the king")
+}
+
+
+
+else
+{
+    console.log("not elegible, you have already gotten one")
+}*/
+
+
+ 
+let firstCard = 10;
+let secondCard = 7;
+let card = 7;
+
+
+let sum = firstCard + secondCard ;
+
+
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el") ;
+let cardsEl = document.querySelector("#cards-el") ;
+
+//if use document.querySelector must be more specific
+//eg: document.queySelector("#sum-el")
+
+
+//let hasBlackJack = false;     //boolean useful to check for state
+//let isAlive = true;
+
+
+//console.log(hasBlackJack) ;
+//console.log(isAlive) ;
+
+//console.log(4===3) ;
+
+
+function startGame()
+{
+        if (sum <= 20)
+        {
+            message = "do you wanna draw a new card?"
+        }
+
+
+
+
+        else if (sum === 21)
+        {
+            message ="wohoo! you got a blackjack"
+            //hasBlackJack = true;
+        }
+
+
+
+
+        else
+        {
+            message="you are out of the game"
+            //isAlive = false;
+        }
+
+
+        messageEl.textContent = message ;
+        sumEl.textContent = "sum: " + sum ;   //or can do it this way `sum: ${sum}`
+        cardsEl.textContent = "Cards: " + firstCard + " " + secondCard ;
+    }
+
+
+
+
+    function newCard()
+    {
+        console.log("drawing anew card from the deck")
+        sumEl.textContent = sum + card;
+        console.log("new sum: " + sum) ;
+    }
